30ae6fb41dfdf3ac29efd4ac47756c72b3f5360d
Nguyen Kien <nguyentrungkien@nguyens-mbp.net.fpt>
2018-09-29 14:16:19 +0700

    d hieu tao dang lam gi

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func viewWillLayoutSubviews() {
+        let dhs = "d hieu tao dang lam gi"
+    }
 }
 
